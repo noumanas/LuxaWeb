77e78137675a858f21f5a10a0663b5f65eff681b
nouman qureshi <noumanqureshi15@gmail.com>
2024-02-06 17:59:12 +0500

    mobile responvoce

--- components/MobileMenuResponsive.tsx
+++ components/MobileMenuResponsive.tsx
@@ -7,7 +7,33 @@ interface mobileMenuResponsive {
   }
 
 const MobileMenuResponsive: React.FC<mobileMenuResponsive> =({ testinfmobileMenu }) => {
- 
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [changeStroke, setchangeStroke] = useState(false);
+  const [activeLinks, setActiveLink] =  useState("services");
+  const [changeStrokeresources, setchangeStrokeresources] = useState(false);
+  const [resourceisDropdownOpen, setresourceisDropdownOpen] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsDropdownOpen(true);
+    setchangeStroke(true)
+  };
+  const closeDropdown = () => {
+    setIsDropdownOpen(false);
+    setchangeStroke(false);
+
+  };
+  const handleMouseEnter2 = () => {
+    setresourceisDropdownOpen(true);
+    setchangeStrokeresources(true);
+
+
+  };
+
+  const closeDropdown2 = () => {
+    setresourceisDropdownOpen(false);
+    setchangeStrokeresources(false);
+
+  };
   return (
     <div className={styles.mobileresponsiveheader}>
     <div className={styles.servicesframe}>
@@ -26,17 +52,109 @@ const MobileMenuResponsive: React.FC<mobileMenuResponsive> =({ testinfmobileMenu
       />
     </div>
     <nav className={styles.alertframe}>
-      <div className={styles.pricingresources}>
-        <div className={styles.services}>Services</div>
-        <img
-          className={styles.stroke3icon}
-          loading="eager"
-          alt=""
-          src="./stroke-3.svg"
-        />
-      </div>
+          <div className={styles.pricingresources} onMouseMove={handleMouseEnter} onMouseLeave={closeDropdown}> 
+            <div className={`${styles.services} ${
+                activeLinks === "services" ? styles.activeLink : ""
+              }`}> 
+              <div className={styles.group}>
+              Services
+              {changeStroke === true ? (
+                <img className={styles.stroke3Icon} alt="" src="/Stroke3-hover.svg" />
+              ):(<img className={styles.stroke3Icon} alt="" src="/stroke-3.svg" />)}
+              
+            </div> 
+          </div>
+          {isDropdownOpen && (
+            <div
+              className={styles.dropdownMenu}
+              onMouseEnter={handleMouseEnter} // Prevent closing when hovering over the dropdown menu
+              onMouseLeave={closeDropdown}
+            >
+                <ul>
+                <Link href="/services">
+                  <li>
+                      Main Services
+                  </li>
+                  </Link>
+                  <Link href="/services/leef-certification-management-software">
+                  <li>
+                      LEED Certification Management Software
+                  </li>
+                  </Link>
+                  <Link href="/services/emission-management-and-reporting-software">
+                  <li>
+                      Scope 1, 2, & 3 Emission Management and Reporting Software
+                  </li>
+                  </Link>
+                  <Link href="/services/data-analytics-insights">
+                  <li>
+                      Data Analytics and Insights
+                  </li>
+                  </Link>
+                </ul>
+              </div>
+            )}
+         </div> 
       <div className={styles.pricing}>Pricing</div>
-      <div className={styles.pricingresources1}>
+            <div className={styles.pricingresources} onMouseMove={handleMouseEnter2} onMouseLeave={closeDropdown2} >
+                <div className={`${styles.services} ${
+                  activeLinks === "resources" ? styles.activeLink : ""
+                }`}><div className={styles.group}>
+                Resources
+                {changeStrokeresources === true ? (
+                  <img className={styles.stroke3Icon} alt="" src="/Stroke3-hover.svg" />
+                ):(<img className={styles.stroke3Icon} alt="" src="/stroke-3.svg" />)}
+                
+              </div> 
+            </div>
+            {resourceisDropdownOpen  && (
+            <div
+              className={styles.dropdownMenu}
+              onMouseEnter={handleMouseEnter2} // Prevent closing when hovering over the dropdown menu
+              onMouseLeave={closeDropdown2}
+            >
+                 
+              <ul>
+              Construction Type
+                <Link href="/resources">
+                  <li>
+                    New Construction - Residential and Commercial
+                  </li>
+                  </Link>
+                  <Link href="/resources/retrofit-and-renovation">
+                  <li>
+                    Retrofit & Renovation
+                  </li>
+                  </Link> 
+                  <Link href="/resources/infrastructure">
+                  <li>
+                    Infrastructure
+                  </li>
+                  </Link>
+                  Industries Type
+                    <Link href="/resources/architecture">
+                  <li>
+                    Architecture
+                  </li>
+                  </Link>
+                  <Link href="/resources/engineering">
+                    <li>
+                      Engineering
+                    </li>
+                  </Link>
+                  <Link href="/resources/construction">
+                    <li>
+                      Construction
+                    </li>
+                  </Link>
+                </ul>
+                  
+                
+              </div>
+            )}
+            </div>  
+
+      {/* <div className={styles.pricingresources}>
         <div className={styles.resources}>Resources</div>
         <img
           className={styles.stroke2icon}
@@ -44,7 +162,7 @@ const MobileMenuResponsive: React.FC<mobileMenuResponsive> =({ testinfmobileMenu
           alt=""
           src="./stroke-2.svg"
         />
-      </div>
+      </div> */}
       <div className={styles.company}>Company</div>
       <div className={styles.pricingresources2}>
         <div className={styles.knowledgecenter}>Knowledge Center</div>
