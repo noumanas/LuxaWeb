f5c3b07014e01d3586338b0bdfd498d044e41586
nouman qureshi <noumanqureshi15@gmail.com>
2024-02-15 18:55:45 +0500

    aaa

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,15 @@
-module.export ={ 
-    app :[ {
-        script: 'npm start'
-    }],
-     deploy :{
-        production :
-        {
-            key: 'luxaweb.pem',
-            user:'ubuntu',
-            host:'16.171.197.128',
-            ref: 'oring/main',
-            repo:'git@github.com:noumanas/LuxaWeb.git',
-            path:'/home/ubuntu',
-            'pre-deploy-local':'', 
-            'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
-            'pre-setup' :'', 
-            'ssh_options':'ForwardAgent=yes'
-        }
-    }
-}
+module.exports = {
+    apps: [
+      {
+        name: 'my-app',
+        script: 'app.js',
+        env: {
+          NODE_ENV: 'development', // Default environment
+        },
+        env_production: {
+          NODE_ENV: 'production', // Production environment
+        },
+      },
+    ],
+  };
+  
